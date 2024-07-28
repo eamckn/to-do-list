33ad8f3174ec84dab9ab40ef5d04f55a269e88d0
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-07-28 14:41:11 -0400

    Initialize files with dependencies

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To-do App',
+            template: './src/index.html'
+        })
+    ],
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist');
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ],
+    },
+}
