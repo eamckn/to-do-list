5caced1709d7b6b6b354d6f93d9a50c539b49a89
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-07-28 21:08:21 -0400

    Add pseudocode planning for initial project needs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
 
 //alert("It's working");
+
+/* Project brainstorm
+
+- I want each to-do item to have a title, description, duedate, and priority listing.
+- I want the creation of each to-do to be a factory that returns an object with those values.
+- I also want a separate function to handle creating each item.
+- I want to first start with making sure that my objects are being created appropriately
+- I'm going to first create the factory that will create and return to-dos, as well as the
+factory that will call the create to-do function
+
+*/
