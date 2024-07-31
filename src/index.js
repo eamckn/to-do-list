e94e530b4c3d5d9116238801254695bcfc3f2f4f
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-07-30 22:46:10 -0400

    Delete unused code for creating of UI

--- src/index.js
+++ src/index.js
@@ -22,78 +22,11 @@ const controller = (function() {
 
     const proj = project();
 
-    const toDoList = [];
-
     const defaultProject = proj.getProject(proj.createProject('name'));
 
     const allProjects = [defaultProject];
 
-    console.log("Here's my empty default project");
-    console.log(defaultProject);
-    console.log("Here's my array of projects:");
-    console.log(allProjects);
-
-    function getTaskFields() {
-        let title = prompt("What would you like to name your task?");
-        let description = prompt("What would you like the task description to be?");
-        let duedate = prompt("When is this task due? Please format your answer as DD/MM/YYYY");
-        let priority = prompt("Is this task high priority, or low? Please respond with either 'high' or 'low'").toLowerCase();
-
-        return { title, description, duedate, priority };
-    }
-
-    function selectProject() {
-        let projectSelection = prompt(
-                                "Would you like to add this task to the default project? Please input 'y' if yes, and 'n' if no");
-        return projectSelection;
-    }
-
-    function determineIfAddingToDefault(todo, projectSelection) {
-
-        if (projectSelection === 'y') {
-            proj.addToProject(defaultProject, todo);
-            console.log(allProjects);
-        }
-
-        else {
-            let projectToAddTo = getProjectToAddTo();
-            findProjectToAddTo(todo, projectToAddTo);
-            console.log(allProjects);
-
-        }
-    }
-
-    function getProjectToAddTo() {
-        let projectToAddTo = prompt("Please enter the name of the project you'd like to add your task to:");
-        return projectToAddTo
-    }
-
-    function findProjectToAddTo(todo, projectToAddTo) {
-        for (const project of allProjects) {
-            if (project.name === projectToAddTo) {
-                proj.addToProject(project, todo);
-                return;
-            }
-        }
-        let answer = prompt("That project doesn't yet exist. Would you like to make a project with that name and add this task to it? Please input 'y' if yes, and 'n' if no.");
-        if (answer === 'y') {
-            let newProject = proj.getProject(proj.createProject(projectToAddTo));
-            proj.addToProject(newProject, todo);
-            allProjects.push(newProject);
-        }
-    }
-
-    function runinConsole() {
-        let todo = getTaskFields();
-        let project = selectProject();
-
-        determineIfAddingToDefault(todo, project);
-
-    }
-
-    runinConsole();
-
-    return { toDoList, allProjects, todo, proj, defaultProject };
+    return { allProjects, todo, proj, defaultProject };
     
 })();
 
