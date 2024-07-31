8f68cb75eb46b079edc83a66df98f4266e533c8d
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-07-30 22:37:20 -0400

    Create rough console version of application
    This is by no means an actually console-functional version of the app,
    but I wanted to test out if the means of creating, accessing, and adding
    projects/tasks worked with the current implementation. From my very
    roughly sketched out console version, it seems like it mostly does. I
    feel as though I've tested a good chunk of the core app logic, and it's
    working, so instead of spending my time creating a really robust and
    nuanced series of console prompts to go through all the possible ways
    of interacting with the app, I should focus on creating the UI for the
    functional logic that is far more intuitive and less reliant on
    elaborate strings of reading input from the console.

--- src/index.js
+++ src/index.js
@@ -18,27 +18,106 @@ factory that will call the create to-do function
 
 const controller = (function() {
 
-    const toDoList = [];
-
-    const allProjects = [];
-
     const todo = toDo();
 
     const proj = project();
 
-    return { toDoList, allProjects, todo, proj };
+    const toDoList = [];
+
+    const defaultProject = proj.getProject(proj.createProject('name'));
+
+    const allProjects = [defaultProject];
+
+    console.log("Here's my empty default project");
+    console.log(defaultProject);
+    console.log("Here's my array of projects:");
+    console.log(allProjects);
+
+    function getTaskFields() {
+        let title = prompt("What would you like to name your task?");
+        let description = prompt("What would you like the task description to be?");
+        let duedate = prompt("When is this task due? Please format your answer as DD/MM/YYYY");
+        let priority = prompt("Is this task high priority, or low? Please respond with either 'high' or 'low'").toLowerCase();
+
+        return { title, description, duedate, priority };
+    }
+
+    function selectProject() {
+        let projectSelection = prompt(
+                                "Would you like to add this task to the default project? Please input 'y' if yes, and 'n' if no");
+        return projectSelection;
+    }
+
+    function determineIfAddingToDefault(todo, projectSelection) {
+
+        if (projectSelection === 'y') {
+            proj.addToProject(defaultProject, todo);
+            console.log(allProjects);
+        }
+
+        else {
+            let projectToAddTo = getProjectToAddTo();
+            findProjectToAddTo(todo, projectToAddTo);
+            console.log(allProjects);
+
+        }
+    }
+
+    function getProjectToAddTo() {
+        let projectToAddTo = prompt("Please enter the name of the project you'd like to add your task to:");
+        return projectToAddTo
+    }
+
+    function findProjectToAddTo(todo, projectToAddTo) {
+        for (const project of allProjects) {
+            if (project.name === projectToAddTo) {
+                proj.addToProject(project, todo);
+                return;
+            }
+        }
+        let answer = prompt("That project doesn't yet exist. Would you like to make a project with that name and add this task to it? Please input 'y' if yes, and 'n' if no.");
+        if (answer === 'y') {
+            let newProject = proj.getProject(proj.createProject(projectToAddTo));
+            proj.addToProject(newProject, todo);
+            allProjects.push(newProject);
+        }
+    }
+
+    function runinConsole() {
+        let todo = getTaskFields();
+        let project = selectProject();
+
+        determineIfAddingToDefault(todo, project);
+
+    }
+
+    runinConsole();
+
+    return { toDoList, allProjects, todo, proj, defaultProject };
     
 })();
 
+//console.log(controller.defaultProject);
+
+/*
 let defaultProject = controller.proj.getProject(controller.proj.createProject("Default"));
 console.log(defaultProject);
 let exampleTask = controller.todo.create("Call mom", "Call my mom. I miss her.", "12/2", "high");
 controller.proj.addToProject(defaultProject, exampleTask);
 console.log(defaultProject);
+*/
 
 /* Further testing for core functionality of adding and updating
 - Create a project
 - Create a task
 - Add task to that project
+
+*/
+
+/* Let's make this work in the console
+
+    - On load, as someone to create a task
+    - One input for name, description, due date, and priority
+    - Ask them if they'd like to add it to the project currently open, or another
 
 */
