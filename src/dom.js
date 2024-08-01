a77f9d1f55f056f0a1ef6d39ef56ee73368e1205
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-08-01 18:17:13 -0400

    Add beginning of dom.js

---
+++ src/dom.js
@@ -0,0 +1,14 @@
+export default function domManip() {
+
+    const content = document.querySelector("#content");
+    const sidebar = document.querySelector("#sidebar");
+    const display = document.querySelector("#display");
+
+    function createTask() {
+
+        let task = document.createElement("div");
+        task.className = "task";
+
+    }
+
+}
